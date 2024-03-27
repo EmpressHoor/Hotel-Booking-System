543dfa986699e5348f4f3ece477c882c2d894351
MAhmeed92111 <f219071@cfd.nu.edu.pk>
2024-03-27 13:06:19 +0500

    EmailVeficationCodeModified

---
+++ emailotp.js
@@ -0,0 +1,35 @@
+function sendOTP() {
+	const email = document.getElementById('email');
+	const otpverify = document.getElementsByClassName('otpverify')[0];
+
+	let otp_val = Math.floor(Math.random() * 10000);
+
+	let emailbody = `<h2>Your OTP is </h2>${otp_val}`;
+	Email.send({
+    SecureToken : "130cbd99-b1b1-48ca-96b6-c7bd6ceb2565",
+    To : email.value,
+    From : "f219071@cfd.nu.edu.pk",
+    Subject : "Email OTP using JavaScript",
+    Body : emailbody,
+}).then(
+
+	message => {
+		if (message === "OK") {
+			alert("OTP sent to your email " + email.value);
+
+			otpverify.style.display = "flex";
+			const otp_inp = document.getElementById('otp_inp');
+			const otp_btn = document.getElementById('otp-btn');
+
+			otp_btn.addEventListener('click', () => {
+				if (otp_inp.value == otp_val) {
+					alert("Email address verified...");
+				}
+				else {
+					alert("Invalid OTP");
+				}
+			})
+		}
+	}
+);
+}
